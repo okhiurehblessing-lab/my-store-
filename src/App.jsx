4e284bb2eb9518f259acf4b2e6c83f11f551d0cb
essysessentials <okhiurehblessing@gmail.com>
2025-08-20 21:28:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-// src/App.jsx
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import StoreFront from "./components/StoreFront";
-import AdminPanel from "./components/AdminPanel";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Checkout from "./pages/Checkout";
+import OrderSuccess from "./pages/OrderSuccess";
+import AdminPanel from "./pages/AdminPanel";
+import Navbar from "./components/Navbar";
+import WhatsAppFloat from "./components/WhatsAppFloat";
 
-export default function App(){
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<StoreFront />} />
-      <Route path="/admin" element={<AdminPanel />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+      <WhatsAppFloat />
+    </>
   );
 }
