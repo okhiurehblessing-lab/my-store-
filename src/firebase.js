8fd94aed287118b67f603e7cc264245c171c0e18
essysessentials <okhiurehblessing@gmail.com>
2025-08-20 22:30:14 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkQ5oE3LaiFGa2ir98MKjZzJ_ZTWQ08Cc",
-  authDomain: "myshop-store-bbb1b.firebaseapp.com",
-  projectId: "myshop-store-bbb1b",
-  storageBucket: "myshop-store-bbb1b.firebasestorage.app",
-  messagingSenderId: "292866448884",
-  appId: "1:292866448884:web:899267f534f8344b086da1"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
