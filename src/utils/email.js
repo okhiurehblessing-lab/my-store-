741772691b4fc96a7790bc1d7b8ef50afd583303
essysessentials <okhiurehblessing@gmail.com>
2025-08-20 22:46:01 +0100

    Update email.js

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,16 +1,10 @@
-// src/utils/email.js
 import emailjs from "emailjs-com";
 
-const SERVICE_ID = "service_opcf6cl";
-const TEMPLATE_CUSTOMER = "template_4zrsdni";
-const TEMPLATE_ADMIN = "template_zc87bdl";
-const PUBLIC_KEY = "RN5H1CcY7Fqkakg5w";
-
-emailjs.init(PUBLIC_KEY);
-
-export async function sendCustomerOrderEmail(vars) {
-  return emailjs.send(SERVICE_ID, TEMPLATE_CUSTOMER, vars);
-}
-export async function sendAdminOrderEmail(vars) {
-  return emailjs.send(SERVICE_ID, TEMPLATE_ADMIN, vars);
-}
+export const sendEmail = (formData) => {
+  return emailjs.send(
+    process.env.REACT_APP_EMAILJS_SERVICE_ID,
+    process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    formData,
+    process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+  );
+};
